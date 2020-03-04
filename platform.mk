8de86743853305ba2e87ca1ad39dc823e02121fd
sjllls <2012ji123@gmail.com>
2020-03-05 02:30:21 +0800

    tone-common: Thermal fix

--- platform.mk
+++ platform.mk
@@ -409,7 +409,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service \
-    thermal.msm8996 \
     thermanager
 
 PRODUCT_COPY_FILES += \
