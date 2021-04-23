e33ccad2728d2aca94325bcb5ab350a1381fda79
sjllls <2012ji123@gmail.com>
2021-04-24 07:27:42 +0800

    tone: Build some vendor blobs
    01-06 05:05:44.621   779   779 F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/netmgrd": library "libhidltransport.so" not found: needed by main executable
    
    Change-Id: Id21e0512ac1236fbb6a5d0e4a6236782dcab7ed5

--- tone.mk
+++ tone.mk
@@ -240,7 +240,11 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhidltransport.vendor \
+    libhwbinder \
+    libhwbinder.vendor
 
 # IMS
 PRODUCT_PACKAGES += \
