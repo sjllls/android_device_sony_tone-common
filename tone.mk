23d284b3d7346bc4cce6bfe6ed64fed5dcf3aac6
sjllls <2012ji123@gmail.com>
2021-11-10 15:58:25 +0800

    Los > AOSP

--- tone.mk
+++ tone.mk
@@ -22,8 +22,7 @@ $(call inherit-product, vendor/sony/tone-common/tone-common-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
@@ -429,10 +428,6 @@ PRODUCT_PACKAGES += \
 # Thermanager
 PRODUCT_PACKAGES += \
     thermanager
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
